8b9caf887f135374304340bb3a4f94f50430d10f
Maloochow <luyao.mly@gmail.com>
2020-09-10 20:51:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const callbackF = (accum, current) => accum + current
+let totalBatteries = batteryBatches.reduce(callbackF, 0)
